c03010a4af388dd204f3b1480524ba1744316653
namdnh <dangngochoainam0601@gmail.com>
2024-12-22 11:56:23 +0700

    grpc done

--- calculator/client/index.js
+++ calculator/client/index.js
@@ -1,6 +1,10 @@
 const grpc = require('@grpc/grpc-js')
 const {CalculatorServiceClient} = require("../proto/calculator_grpc_pb");
-const {SumRequest} = require('../proto/sum_pb');
+const {PrimesRequest} = require('../proto/primes_pb');
+const {AvgRequest} = require('../proto/avg_pb');
+const {MaxRequest} = require('../proto/max_pb');
+const {DeadlineRequest} = require('../proto/deadline_pb');
+
 
 const doSum = (client) => {
     console.log('doSum was invoked');
@@ -15,12 +19,76 @@ const doSum = (client) => {
     })
 }
 
+const doPrimes = (client) => {
+    console.log('doPrimes was invoked');
+    const req = new PrimesRequest();
+
+    req.setNumber(120);
+    const call = client.primes(req);
+
+    call.on('data', (res) => {
+        console.log(`Primes: ${res.getOutput()}`);
+    });
+}
+
+const doAvg = (client) => {
+    console.log('doAvg was invoked');
+
+    const listNumber = [1,2,3,4];
+
+    const call = client.avg((err, res) => {
+        if (err) {
+            console.error(err)
+        }
+        console.log(`Sum: ${res.getOutput()}`);
+    })
+
+    listNumber.forEach(number => call.write(new AvgRequest().setNumber(number)))
+
+    call.end()
+
+}
+
+const doMax = (client) => {
+    console.log('doMax was invoked');
+
+    const call = client.max()
+
+    call.on('data', (res) => {
+        console.log(`Max: ${res.getOutput()}`);
+    })
+    const listNumber = [1,5,3,6,2,20]
+
+    listNumber.forEach(number => call.write(new MaxRequest().setNumber(number)))
+
+    call.end()
+}
+
+const doDeadline = (client, ms) => {
+    console.log('doDeadline was invoked');
+    const req = new DeadlineRequest().setNumber(3)
+
+    client.deadline(req, {
+        deadline: new Date(Date.now() + ms),
+    }, (err, res) => {
+        if (err) {
+            console.error(err)
+        }
+
+        console.log(`No Deadline: ${res.getOutput()}`);
+    })
+}
+
 function main() {
     const client = new CalculatorServiceClient('localhost:50052',
         grpc.credentials.createInsecure());
 
-    doSum(client)
-
+    // doSum(client)
+    // doPrimes(client)
+    // doAvg(client)
+    // doMax(client)
+    doDeadline(client, 1000)
+    // doDeadline(client, 4000)
     // client.close()
 }
 
--- calculator/server/service_impl.js
+++ calculator/server/service_impl.js
@@ -1,4 +1,10 @@
 const {SumResponse} = require('../proto/sum_pb')
+const {PrimesResponse} = require('../proto/primes_pb')
+const {AvgResponse} = require('../proto/avg_pb')
+const {MaxResponse} = require('../proto/max_pb')
+const {DeadlineResponse} = require('../proto/deadline_pb')
+
+
 
 exports.sum = (call, callback) => {
     console.log('Sum was invoked');
@@ -8,4 +14,83 @@ exports.sum = (call, callback) => {
     const res = new SumResponse().setResult(result)
 
     callback(null, res);
+}
+
+exports.primes = (call, _) => {
+    console.log('Primes was invoked');
+    let number = call.request.getNumber();
+    let divisor = 2;
+
+    while (number > 1) {
+        const res = new PrimesResponse();
+        if (number % divisor == 0) {
+            res.setOutput(divisor);
+            call.write(res);
+            number /= divisor;
+        } else {
+            ++divisor;
+        }
+    }
+
+    call.end();
+};
+
+
+exports.avg = (call, callback) => {
+    console.log('Avg was invoked');
+
+    const listNumber = []
+
+    call.on('data', (req) => {
+        listNumber.push(req.getNumber());
+    })
+
+    call.on('end', () => {
+        const avg = listNumber.reduce((acc, number) => acc + number, 0)/listNumber.length;
+        const res = new AvgResponse().setOutput(avg);
+        callback(null, res);
+    })
+}
+
+exports.max = (call, callback) => {
+    console.log('Max was invoked');
+
+    const output = []
+    let max = 0;
+
+    call.on('data', (req) => {
+        const number = req.getNumber();
+        const findMax = Math.max(number, ...output)
+        if (findMax > max) {
+            max = findMax;
+            output.push(number);
+            call.write(new MaxResponse().setOutput(number));
+        }
+    })
+
+    call.on('end', () => {
+        console.log(output)
+        call.end()
+    })
+}
+
+exports.deadline = async (call, callback) => {
+    console.log('Deadline was invoked');
+
+    const result = call.request.getNumber()
+
+    for (let i = 0; i < result; i++) {
+        if (call.cancelled) {
+            return console.log('The client cancelled the request!');
+        }
+        await delay(1000)
+    }
+
+    const res = new DeadlineResponse().setOutput(1)
+
+    callback(null, res);
+}
+
+const delay = (ms) => {
+    return new Promise(rs => setTimeout(rs, ms));
 }
